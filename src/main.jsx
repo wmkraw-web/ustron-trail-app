dba80ab8deaffa4a120ec618a9f850413bb257c8
wmkraw-web <wmkraw@gmail.com>
2026-05-23 00:48:17 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Jeśli masz plik CSS, jeśli nie - usuń tę linię
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
