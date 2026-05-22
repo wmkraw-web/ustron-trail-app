c925719b716e55f74cf56a0589e1c6c18cdf90b0
wmkraw-web <wmkraw@gmail.com>
2026-05-23 00:40:46 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,4 @@
 const CACHE_NAME = 'beskidy-cache-v1';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
-      ]);
-    })
-  );
-});
-
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
