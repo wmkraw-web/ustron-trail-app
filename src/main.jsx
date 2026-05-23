756981237f79fc2156e16ff8d67e1c86cab6fc24
wmkraw-web <wmkraw@gmail.com>
2026-05-23 10:14:04 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// Jeśli nie masz pliku index.css, Vite to po prostu zignoruje
-// Ale dla pewności usuniemy ten import, a cały Tailwind obsłużymy w App.jsx 
-// poprzez cdn, jeśli nie instalowałeś tailwinda lokalnie. 
-// Standardowo Vite wymagałoby lokalnego Tailwinda.
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
